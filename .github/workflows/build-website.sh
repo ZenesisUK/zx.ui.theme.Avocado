9a01cb19289ed28860219aac927bb74029933ae8
Patryk Milinowski <pmilinowski@vmn.digital>
2023-11-23 10:57:49 +0000

    deploy website workflow

---
+++ .github/workflows/build-website.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DEPLOY_PK="$1"
+
+mkdir tmp
+ABSOLUTE_TMP=$(echo "$(cd "$(dirname "$tmp")"; pwd -P)/$(basename "tmp")")
+echo "$DEPLOY_PK" > ./tmp/deploy-key
+chmod 0600 ./tmp/deploy-key
+npm i
+
+REPO_NAME="zx-ui-theme-avocado.github.io"
+
+echo ">>> Cloning existing website..."
+git config --global user.email "deployment@qooxdoo.org"
+git config --global user.name "Automated Deployment for qooxdoo/qxl-datagrid.qooxdoo.github.io"
+git clone -c core.sshCommand="/usr/bin/ssh -i $ABSOLUTE_TMP/deploy-key" git@github.com:zenesisuk/$REPO_NAME.git --depth=1 ./tmp/$REPO_NAME
+
+echo
+echo ">>> Building website..."
+npx qx deploy --out=./tmp/$REPO_NAME
+
+cd ./tmp/$REPO_NAME
+if [[ ! -d .git ]] ; then
+    echo "The checked out $REPO_NAME is not a .git repo!"
+    exit 1
+fi
+
+git add .
+git commit -m 'automatic deployment from ZenesisUk/zx.ui.theme.avocado/.github/workflows/build-website.sh'
+git push
